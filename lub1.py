97ed5c7bd09d9170da502cf75ab289db8286c6c7
Філь Тімур <fil.tumyr@gmail.com>
2025-12-02 19:26:17 +0200

    Modify personal details and clean up code
    Updated height and name variables, removed arithmetic operations, and added print statement for numbers.

---
+++ lub1.py
@@ -0,0 +1,16 @@
+age=16
+height=190.0
+name="Timur"
+student=True
+student_in_the_group=26
+grades=[4, 5, 5, 4]
+digits=(1, 2, 3, 4, 5, 6, 7, 8, 9)
+numbers={10, 20, 30, 40, 50}
+print('age:', age, type(age))
+print('height:', height, type(height))
+print('name:', name, type(name))
+print('student:', student, type(student))
+print('student_in_the_group:', student_in_the_group, type(student_in_the_group))
+print('grades:', grades, type(grades))
+print('digits:', digits, type(digits))
+print('numbers:', numbers, type(numbers))
